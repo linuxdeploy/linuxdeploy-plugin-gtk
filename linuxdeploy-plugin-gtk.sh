01ae4f235585202e1514c7d91d7e0dbb8914fe9d
FabianLars <fabianlars@fabianlars.de>
2021-12-10 21:45:47 +0100

    add missing tls backend. overwrite gio modules dir

--- linuxdeploy-plugin-gtk.sh
+++ linuxdeploy-plugin-gtk.sh
@@ -237,5 +237,16 @@ done
 
 # set write permission on lib64 again to make it deletable.
 chmod +w "$APPDIR"/usr/lib64 || true
+
+# We have to copy the files first to not get permission errors when we assign gio_extras_dir
+find /usr/lib* -name libgiognutls.so -exec mkdir -p "$APPDIR"/$(dirname '{}') \; -exec cp --parents '{}' "$APPDIR/" \; || true
+# related files that we seemingly don't need:
+# libgiolibproxy.so - libgiognomeproxy.so - glib-pacrunner
+
+gio_extras_dir=$(find "$APPDIR"/usr/lib* -name libgiognutls.so -exec dirname '{}' \; 2>/dev/null)
+cat >> "$HOOKFILE" <<EOF
+export GIO_EXTRA_MODULES="\$APPDIR/${gio_extras_dir#$APPDIR/}"
+EOF
+
 #binary patch absolute paths in libwebkit files
 find $APPDIR/usr/lib* -name libwebkit* -exec sed -i -e "s|/usr|././|g" '{}' \;
