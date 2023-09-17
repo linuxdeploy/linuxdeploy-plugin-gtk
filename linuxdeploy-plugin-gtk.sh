221b680aeabb51d103fffc97f9d550373bd52fa0
Fushan Wen <qydwhotmail@gmail.com>
2023-09-17 19:50:05 +0800

    Prefer color-scheme from org.freedesktop.portal.Desktop
    In some distros GNOME 42 or later versions are not available, thus
    querying color-scheme will return an error.

--- linuxdeploy-plugin-gtk.sh
+++ linuxdeploy-plugin-gtk.sh
@@ -226,10 +226,14 @@ mkdir -p "$HOOKSDIR"
 cat > "$HOOKFILE" <<\EOF
 #! /usr/bin/env bash
 
-case "$(gsettings get org.gnome.desktop.interface color-scheme 2> /dev/null)" in
-    "'prefer-dark'")  GTK_THEME_VARIANT="dark";;
-    "'prefer-light'") GTK_THEME_VARIANT="light";;
-    *)                GTK_THEME_VARIANT="light";;
+COLOR_SCHEME="$(dbus-send --session --dest=org.freedesktop.portal.Desktop --type=method_call --print-reply --reply-timeout=1000 /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read 'string:org.freedesktop.appearance' 'string:color-scheme' 2>/dev/null | tail -n1 | cut -b35- | cut -d' ' -f2)"
+if [ -z "$COLOR_SCHEME" ]; then
+    COLOR_SCHEME="$(gsettings get org.gnome.desktop.interface color-scheme 2> /dev/null)"
+fi
+case "$COLOR_SCHEME" in
+    "1"|"'prefer-dark'")  GTK_THEME_VARIANT="dark";;
+    "2"|"'prefer-light'") GTK_THEME_VARIANT="light";;
+    *)                    GTK_THEME_VARIANT="light";;
 esac
 APPIMAGE_GTK_THEME="${APPIMAGE_GTK_THEME:-"Adwaita:$GTK_THEME_VARIANT"}" # Allow user to override theme (discouraged)
 
